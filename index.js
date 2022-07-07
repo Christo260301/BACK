50d48c05bec0aba6a0f94887c18ff7e92d969a75
Christo260301 <christo.260301@hotmail.com>
2022-07-07 01:17:54 -0500

    OO

--- index.js
+++ index.js
@@ -2,14 +2,14 @@
 require('./config/conexion');
 
 const express = require('express');
-const port = (process.env.port || 8080);
+const PORT = (process.env.PORT || 8080);
 const app = express();
 
 //Este fragmento admite el tipo de dato
 app.use(express.json())
 
 //Se configura el puerto donde se ejecutara el servidor
-app.set('port',port);
+app.set('port',PORT);
 
 //Rutas que seran utilizadas 
 app.use('/api', require('./rutas'));
@@ -19,6 +19,6 @@ app.listen(app.get('port'),(error)=>{
     if(error){
         console.log('Error al iniciar el servidor: ' + error)
     } else {
-        console.log('Servidor iniciado: ' + port )
+        console.log('Servidor iniciado: ' + PORT )
     }
 });
