dbead0aea8a1d2cd1205bacc349cfa1097c8f768
Christo260301 <christo.260301@hotmail.com>
2022-07-07 02:57:03 -0500

    3000

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 require('./config/conexion');
 
 const express = require('express');
-const PORT = (process.env.PORT || 8080);
+const PORT = (process.env.PORT || 3000);
 const app = express();
 
 //Este fragmento admite el tipo de dato
